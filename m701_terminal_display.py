dc03fdba2840c64bb40ea233d953b6518dc42140
Sh4peshifting <56795584+Sh4peshifting@users.noreply.github.com>
2023-11-14 16:34:50 +0800

    Update m701_terminal_display.py

--- m701_terminal_display.py
+++ m701_terminal_display.py
@@ -2,6 +2,7 @@ import serial
 import time
 import curses
 
+thresholds = {'eCO2': 4000, 'eCH2O': 15, 'TVOC': 20, 'PM2.5': 50, 'PM10': 70, 'Temperature': 25, 'Humidity': 35}
 
 def read_m701_data(serial_port):
     # Read response
@@ -29,59 +30,52 @@ def read_m701_data(serial_port):
 
     return data
 
-def create_curses_gui():
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
-    return stdscr
-
-def update_curses_gui(stdscr, data):
-    stdscr.clear()
+def display_data(window, data):
+    window.clear()
     for i, (key, value) in enumerate(data.items()):
-        stdscr.addstr(i, 0, f"{key}: {value}")
-    stdscr.refresh()
+        window.addstr(i, 0, f'{key}: {value}')
+        if value > thresholds[key]:
+            window.addstr(len(data), 0, f'警告：{key}数值超过阈值！')
+    window.refresh()
 
-def check_curses_thresholds(stdscr, data, thresholds):
-    warnings = []
-    for key, value in data.items():
-        if key in thresholds and value > thresholds[key]:
-            warnings.append(f"{key}数值超过阈值")
-    if warnings:
-        stdscr.addstr(len(data), 0, f"警告：{'，'.join(warnings)}")
-    stdscr.refresh()
-
-def input_thresholds(stdscr, data):
-    thresholds = {}
-    stdscr.clear()
-    for i, key in enumerate(data.keys()):
-        stdscr.addstr(i, 0, f"输入{key}的阈值：")
-        curses.echo()
-        thresholds[key] = float(stdscr.getstr(i, len(f"输入{key}的阈值：")).decode())
-        curses.noecho()
-    return thresholds
+def set_threshold(window):
+    global thresholds
+    keys = list(thresholds.keys())
+    current = 0
+    while True:
+        window.clear()
+        for i, key in enumerate(keys):
+            if i == current:
+                window.addstr(i, 0, f'{key}: {thresholds[key]}', curses.A_REVERSE)
+            else:
+                window.addstr(i, 0, f'{key}: {thresholds[key]}')
+        window.refresh()
+        c = window.getch()
+        if c == curses.KEY_UP:
+            current = (current - 1) % len(keys)
+        elif c == curses.KEY_DOWN:
+            current = (current + 1) % len(keys)
+        elif c == ord('\n'):
+            window.addstr(len(keys), 0, '输入新的阈值：')
+            window.refresh()
+            curses.echo()
+            thresholds[keys[current]] = int(window.getstr(len(keys), 0))
+            curses.noecho()
+            break
 
 def main():
     serial_port = serial.Serial('/dev/ttyS4', 9600, timeout=1)
-    stdscr = create_curses_gui()
-    thresholds = {}
-    try:
-        while True:
-            data = read_m701_data(serial_port)
-            if data:
-                update_curses_gui(stdscr, data)
-                check_curses_thresholds(stdscr, data, thresholds)
-            if stdscr.getch() == ord('i'):
-                thresholds = input_thresholds(stdscr, data)
-            time.sleep(1)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
-
+    window = curses.initscr()
+    curses.noecho()
+    window.keypad(True)
+    while True:
+        data = read_m701_data(serial_port)
+        if data is not None:
+            display_data(window, data)
+        c = window.getch()
+        if c == ord('i'):
+            set_threshold(window)
+    curses.endwin()
 
 
 if __name__ == '__main__':
