3c2e86d69f6dbbc16464a6d35231a9941076a1b2
Sh4peshifting <56795584+Sh4peshifting@users.noreply.github.com>
2023-11-14 17:26:17 +0800

    Update m701_data_display.py

--- m701_data_display.py
+++ m701_data_display.py
@@ -1,7 +1,12 @@
 import serial
 import time
-import tkinter as tk
+import curses
 
+# 初始化串口
+ser = serial.Serial('/dev/ttyS4', 9600, timeout=1)
+
+# 初始化阈值
+thresholds = {'eCO2': 4000, 'eCH2O': 15, 'TVOC': 20, 'PM2.5': 50, 'PM10': 70, 'Temperature': 25, 'Humidity': 35}
 
 def read_m701_data(serial_port):
     # Read response
@@ -29,46 +34,74 @@ def read_m701_data(serial_port):
 
     return data
 
-def create_gui():
-    window = tk.Tk()
-    window.title("传感器数据")
-    labels = ['eCO2含量', 'eCH2O含量', 'TVOC含量', 'PM2.5含量', 'PM10含量', '温度', '湿度']
-    for i, label in enumerate(labels):
-        tk.Label(window, text=f"{label}：").grid(row=i, column=0)
-        tk.Label(window, text="设定阈值：").grid(row=i, column=2)
-        tk.Entry(window).grid(row=i, column=1)  # Data display
-        tk.Entry(window).grid(row=i, column=3)  # Threshold input
-    tk.Label(window, text="警告：").grid(row=8, column=0)
-    return window
-
-def update_gui(window, data):
-    for i, value in enumerate(data.values()):
-        window.grid_slaves(row=i, column=1)[0].delete(0, tk.END)
-        window.grid_slaves(row=i, column=1)[0].insert(0, str(value))
-
-def check_thresholds(window, data):
+def display_data(stdscr, data):
     warnings = []
+    stdscr.clear()
+    stdscr.addstr(0, 0, "CO2含量: {} ppm".format(data['eCO2']))
+    stdscr.addstr(1, 0, "CH2O含量: {} ug/m3".format(data['eCH2O']))
+    stdscr.addstr(2, 0, "TVOC含量: {} ug/m3".format(data['TVOC']))
+    stdscr.addstr(3, 0, "PM2.5含量: {} ug/m3".format(data['PM2.5']))
+    stdscr.addstr(4, 0, "PM10含量: {} ug/m3".format(data['PM10']))
+    stdscr.addstr(5, 0, "温度: {} ℃".format(data['Temperature']))
+    stdscr.addstr(6, 0, "湿度: {} %".format(data['Humidity']))
     for i, (key, value) in enumerate(data.items()):
-        threshold = window.grid_slaves(row=i, column=3)[0].get()
-        if threshold and value > float(threshold):
-            warnings.append(f"{key}数值超过阈值")
-    window.grid_slaves(row=8, column=0)[0].config(text=f"警告：{'，'.join(warnings)}")
+        if value > thresholds[key]:
+            warnings.append(key)
+    if len(warnings) > 0:
+            stdscr.addstr(7, 0, f'警告：{", ".join(warnings)}超过阈值')
+    else:
+        stdscr.addstr(7, 0, '空气指标正常')
+    stdscr.refresh()
 
-def main():
-    serial_port = serial.Serial('/dev/ttyS4', 9600, timeout=1)
-    window = create_gui()
+def handle_input(stdscr, data):
+    c = stdscr.getch()
+    if c == ord('i'):
+        # 暂停数据接收，设置阈值
+        selected = 0
+        while True:
+            stdscr.clear()
+            for i, key in enumerate(data.keys()):
+                if i == selected:
+                    stdscr.addstr(i, 0, "{}: {} ".format(key, data[key]), curses.A_REVERSE)
+                else:
+                    stdscr.addstr(i, 0, "{}: {} ".format(key, data[key]))
+            stdscr.addstr(7, 0, "按上下键选择阈值，按回车确认")
+            stdscr.refresh()
+            c = stdscr.getch()
+            if c == curses.KEY_UP:
+                selected = (selected - 1) % len(data)
+            elif c == curses.KEY_DOWN:
+                selected = (selected + 1) % len(data)
+            elif c == curses.KEY_ENTER or c == 10 or c == 13:
+                for key in data.keys():
+                    if key == list(data.keys())[selected]:
+                        stdscr.addstr(8, 0, "{} 阈值: ".format(key))
+                        stdscr.refresh()
+                        threshold = ''
+                        while True:
+                            c = stdscr.getch()
+                            if c == curses.KEY_ENTER or c == 10 or c == 13:
+                                if len(threshold) <= 4:
+                                    thresholds[key] = int(threshold)
+                                    break
+                            elif c >= ord('0') and c <= ord('9'):
+                                if len(threshold) < 4:
+                                    threshold += chr(c)
+                                    stdscr.addstr(8, len("{} 阈值: ".format(key)) + len(threshold), chr(c))
+                                    stdscr.refresh()
+
+                break
+            elif c == 27:  # esc键
+                break
+
+def main(stdscr):
     while True:
-        try:
-            data = read_m701_data(serial_port)
-            if data:
-                update_gui(window, data)
-                check_thresholds(window, data)
-            window.update()
-            time.sleep(1)
-        except Exception as e:
-            print(f"Error: {e}")
-            time.sleep(1)
-
+        # 读取串口数据
+        data = read_m701_data(ser)
+        # 显示数据
+        display_data(stdscr, data)
+        # 处理键盘输入
+        handle_input(stdscr, thresholds)
 
 if __name__ == '__main__':
-    main()
+    curses.wrapper(main)
